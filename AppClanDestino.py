4b2cc51c25fbde444063aced62d868a80785dd09
alejandrofernandezurbano <120064413+alejandrofernandezurbano@users.noreply.github.com>
2026-02-26 17:26:13 -0500

    Update AppClanDestino.py

--- AppClanDestino.py
+++ AppClanDestino.py
@@ -222,5 +222,6 @@ def delete_pegue(doc_id):
 
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
+    #app.run(host="0.0.0.0", port=5000)
     #app.run(host="0.0.0.0", port=5000, debug=True)
