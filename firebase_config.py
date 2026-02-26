df0516d2cecde2974294540f4698a320858d97b8
alejandrofernandezurbano <120064413+alejandrofernandezurbano@users.noreply.github.com>
2026-02-26 17:07:48 -0500

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,11 +3,11 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 
-#cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 
 
-cred_dict = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
-cred = credentials.Certificate(cred_dict)
+#cred_dict = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
+#cred = credentials.Certificate(cred_dict)
 
 firebase_admin.initialize_app(cred)
 db = firestore.client()
