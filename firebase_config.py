a6dbb78be89650be99613c15d25f48628da417d0
alejandrofernandezurbano <120064413+alejandrofernandezurbano@users.noreply.github.com>
2026-02-26 17:17:07 -0500

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,11 +3,23 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
 
-cred = credentials.Certificate("serviceAccountKey.json")
+base64_key = os.environ.get("FIREBASE_KEY_BASE64")
+
+if not base64_key:
+    raise ValueError("No FIREBASE_KEY_BASE64 environment variable set")
+
+decoded_key = base64.b64decode(base64_key)
+cred_dict = json.loads(decoded_key)
+
+cred = credentials.Certificate(cred_dict)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+'''cred = credentials.Certificate("serviceAccountKey.json")
 
 
 #cred_dict = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
 #cred = credentials.Certificate(cred_dict)
 
 firebase_admin.initialize_app(cred)
-db = firestore.client()
+db = firestore.client()'''
